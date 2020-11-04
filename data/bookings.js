586ab370af9d6ac78197b48741902b4b980d886f
Cameron Romo <cromo1225@gmail.com>
2020-11-04 15:14:29 -0700

    fix data

--- data/bookings.js
+++ data/bookings.js
@@ -1,119 +1,126 @@
   export const bookings = [
   {
   "id": "5fwrgu4i7k55hl6sz",
-  "userID": 9,
+  "userID": 1,
   "date": "2020/04/22",
-  "roomNumber": 15,
+  "roomNumber": 5,
+  "roomServiceCharges": []
+  },
+  {
+  "id": "5fwrgu4i7k55hl6sz",
+  "userID": 1,
+  "date": "2020/04/22",
+  "roomNumber": 4,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6t5",
-  "userID": 43,
+  "userID": 2,
   "date": "2020/01/24",
   "roomNumber": 24,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6t6",
-  "userID": 13,
+  "userID": 1,
   "date": "2020/01/10",
-  "roomNumber": 12,
+  "roomNumber": 3,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6t7",
-  "userID": 20,
+  "userID": 2,
   "date": "2020/02/16",
   "roomNumber": 7,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6t8",
-  "userID": 1,
+  "userID": 2,
   "date": "2020/02/05",
   "roomNumber": 12,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6t9",
-  "userID": 38,
+  "userID": 2,
   "date": "2020/02/14",
   "roomNumber": 14,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6ta",
-  "userID": 25,
+  "userID": 3,
   "date": "2020/01/11",
   "roomNumber": 9,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6tb",
-  "userID": 49,
+  "userID": 3,
   "date": "2020/02/06",
   "roomNumber": 5,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6tc",
-  "userID": 22,
+  "userID": 3,
   "date": "2020/01/30",
   "roomNumber": 13,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6td",
-  "userID": 27,
+  "userID": 4,
   "date": "2020/01/31",
   "roomNumber": 20,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6te",
-  "userID": 44,
+  "userID": 4,
   "date": "2020/01/19",
   "roomNumber": 8,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6tf",
-  "userID": 36,
+  "userID": 4,
   "date": "2020/01/25",
   "roomNumber": 2,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6tg",
-  "userID": 34,
+  "userID": 5,
   "date": "2020/02/03",
   "roomNumber": 17,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6th",
-  "userID": 19,
+  "userID": 5,
   "date": "2020/02/26",
   "roomNumber": 15,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6ti",
-  "userID": 6,
+  "userID": 5,
   "date": "2020/01/22",
   "roomNumber": 11,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6tj",
-  "userID": 21,
+  "userID": 6,
   "date": "2020/01/17",
   "roomNumber": 7,
   "roomServiceCharges": []
   },
   {
   "id": "5fwrgu4i7k55hl6tk",
-  "userID": 7,
+  "userID": 6,
   "date": "2020/01/27",
   "roomNumber": 20,
   "roomServiceCharges": []
