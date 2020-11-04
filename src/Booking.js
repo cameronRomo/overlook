6bc66bd026dba7fee40c94b5b790750f78a05195
Cameron Romo <cromo1225@gmail.com>
2020-11-04 15:20:59 -0700

    Add properties for Booking

---
+++ src/Booking.js
@@ -0,0 +1,9 @@
+export default class Booking {
+  constructor(bookingDetails) {
+    this.id = bookingDetails.id;
+    this.userID = bookingDetails.userID;
+    this.date = bookingDetails.date;
+    this.roomNumber = bookingDetails.roomNumber;
+    this.roomServiceCharges = bookingDetails.roomServiceCharges || [];
+  }
+}
