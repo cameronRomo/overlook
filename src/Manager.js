3286d4df3caf111040187a54f76df8e9f669af3c
Cameron Romo <cromo1225@gmail.com>
2020-11-04 19:37:20 -0700

    Add properties and methods for Manager

---
+++ src/Manager.js
@@ -0,0 +1,42 @@
+import User from '../src/User'
+export default class Manager extends User {
+  constructor(userDetails) {
+    super(userDetails);
+  }
+
+  searchUsers(users, name) {
+    let foundUser = users.find(user => {
+      return user.name === name;
+    })
+    return new User(foundUser);
+  }
+
+  viewUserBookings(bookingsList, users, name) {
+    let user = this.searchUsers(users, name);
+    return user.viewBookings(bookingsList);
+  }
+
+  addBooking(users, name, date, roomNumber) {
+    let user = this.searchUsers(users, name);
+    return user.makeBooking(user.id, date, roomNumber);
+  }
+
+  totalRoomsAvailable(bookingsDetails, roomsDetails, date) {
+    return this.checkAvailability(bookingsDetails, roomsDetails, date).length;
+  }
+
+  calculatePercentOccupied(bookingsDetails, roomsDetails, date) {
+    let unavailableRooms = this.noVacancy(bookingsDetails, roomsDetails, date).length
+    let ratio = unavailableRooms / roomsDetails.length
+    let percent = ratio * 100;
+    return `${percent}% of rooms are occupied`
+  }
+
+  totalRevenue(bookingsDetails, roomsDetails, date) {
+    let roomsAccountedFor = this.noVacancy(bookingsDetails, roomsDetails, date)
+    return Number(roomsAccountedFor.reduce((totalIncoming, room) => {
+      totalIncoming += room.costPerNight;
+      return totalIncoming;
+    }, 0).toFixed(2))
+  }
+}
