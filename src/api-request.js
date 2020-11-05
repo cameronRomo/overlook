615759183cf30d681c9221670dc21a83dd5c835b
Cameron Romo <cromo1225@gmail.com>
2020-11-05 11:36:44 -0700

    Add apiRequest object literal and export

---
+++ src/api-request.js
@@ -0,0 +1,22 @@
+export const apiRequest = {
+  getUsersData() {
+    return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+      .then(response => response.json())
+      .then(data => data.users)
+      .catch(error => console.log(error));
+  },
+
+  getRoomsData() {
+    return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+      .then(response => response.json())
+      .then(data => data.rooms)
+      .catch(error => console.log(error));
+  },
+
+  getBookingsData() {
+    return fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+      .then(response => response.json())
+      .then(data => data.bookings)
+      .catch(error => console.log(error));
+  }
+};
