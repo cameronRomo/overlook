43f4e4f03976be9311b2ce6541ce5e475e0032f1
Cameron Romo <cromo1225@gmail.com>
2020-11-06 19:41:39 -0700

    Promise all incoming data

--- src/index.js
+++ src/index.js
@@ -8,4 +8,28 @@ import './css/base.scss';
 import './images/user.svg'
 import './images/The_Grand_Budapest_Hotel_-_Original_Soundtrack.jpg'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+import Booking from './Booking';
+import User from './User';
+import Manager from './Manager';
+
+import apiRequest from './api-request';
+
+let userData;
+let roomData;
+let bookingData;
+
+let currentUser;
+let manager;
+
+const recievedUsersData = apiRequest.getUsersData();
+const recievedRoomsData = apiRequest.getRoomsData();
+const recievedBookingsData = apiRequest.getBookingsData();
+
+Promise.all([recievedUsersData, recievedRoomsData, recievedBookingsData])
+  .then(value => {
+    userData = value[0];
+    roomData = value[1];
+    bookingData = value[2];
+    //startApp
+  })
+  .catch(error => console.log(error));
