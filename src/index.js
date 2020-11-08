c405948509535551b3faf261dcd6602c857d1deb
Cameron Romo <cromo1225@gmail.com>
2020-11-08 14:01:11 -0700

    Add functions for the manager dashboard view

--- src/index.js
+++ src/index.js
@@ -18,11 +18,19 @@ let roomData;
 let bookingData;
 
 let currentUser;
-let manager;
+// let manager;
 
 const usernameInput = document.querySelector('#username');
 const userPasswordInput = document.querySelector('#password');
 const signInButton = document.querySelector('.modal__login__button');
+const managerAvailability = document.querySelector('.body__manager__availability__count');
+const revenueForDay = document.querySelector('.body__manager__revenue');
+const percentTaken = document.querySelector('.body__manager__percent__occupied');
+const managerDash = document.querySelector('.body__manager');
+const nav = document.querySelector('.nav');
+const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal__overlay');
+const userDrop = document.querySelector('#user-drop');
 
 signInButton.addEventListener('click', validateLogin);
 
@@ -42,20 +50,61 @@ Promise.all([recievedUsersData, recievedRoomsData, recievedBookingsData])
 
 
 function startApp() {
-
+  showUsers();
 }
 
 function validateLogin(event) {
   event.preventDefault();
-  if(usernameInput.value === 'manager' && userPasswordInput.value === 'overlook2020') {
+  if (usernameInput.value === 'manager' && userPasswordInput.value === 'overlook2020') {
     currentUser = new Manager();
-    // helper function to show customer dashboard
+    openManagerDash();
+    console.log(currentUser);
+    // helper funtion to show manager dashboard
   } else if (usernameInput.value.slice(0, 8) === 'customer' && userPasswordInput.value === 'overlook2020') {
     let currentId = Number(usernameInput.value.slice(8));
     let userById = userData.find(user => user.id === currentId);
     currentUser = new User(userById)
-    // helper funtion to show manager dashboard
-  }else {
+    // helper function to show customer dashboard
+  } else {
     alert("Appologies, the username or password you have entered does not match any we have on file. Please try again!")
   }
+}
+
+function openManagerDash() {
+  managerDash.classList.remove('hidden');
+  nav.classList.remove('hidden');
+  modal.classList.add('hidden');
+  modalOverlay.classList.add('hidden');
+  numberOfRoomsAvailable(bookingData, roomData, "2020/04/22");
+  todaysRevenue(bookingData, roomData, "2020/04/22");
+  percentOccupied(bookingData, roomData, "2020/04/22");
+}
+
+function numberOfRoomsAvailable(bookings, rooms, date) {
+  let totalNotOccupied = 25 - currentUser.totalRoomsAvailable(bookings, rooms, date);
+  managerAvailability.innerText = `${totalNotOccupied} are available.`;
+}
+
+function todaysRevenue(bookings, rooms, date) {
+  let revenue = currentUser.totalRevenue(bookings, rooms, date);
+  revenueForDay.innerText = `Total revenue ${revenue}`;
+}
+function percentOccupied(bookings, rooms, date) {
+  let daysPercentage = currentUser.calculatePercentOccupied(bookings, rooms, date);
+  percentTaken.innerText = daysPercentage;
+}
+
+function chooseUser(option) {
+  // not working, seems to be undefined in the html...
+  newUser = userData.find(user => option.value === user.name);
+  currentUser = new User(newUser);
+  console.log(currentUser);
+}
+
+function showUsers() {
+  let userDropDownList = userData.reduce((usersHTML, user) => {
+    usersHTML += `<option class='nav__div__one__dropdown__choice' value='${user.name}'>${user.name}</option>`;
+    return usersHTML;
+  }, '')
+  document.querySelector('#user-drop').insertAdjacentHTML('afterend', userDropDownList)
 }
