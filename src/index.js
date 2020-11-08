d74614d9728ce577a4baa6d0ba64c74dae6a0910
Cameron Romo <cromo1225@gmail.com>
2020-11-07 18:39:34 -0700

    Add starting reference for the next feature

--- src/index.js
+++ src/index.js
@@ -49,22 +49,13 @@ function validateLogin(event) {
   event.preventDefault();
   if(usernameInput.value === 'manager' && userPasswordInput.value === 'overlook2020') {
     currentUser = new Manager();
+    // helper function to show customer dashboard
   } else if (usernameInput.value.slice(0, 8) === 'customer' && userPasswordInput.value === 'overlook2020') {
     let currentId = Number(usernameInput.value.slice(8));
     let userById = userData.find(user => user.id === currentId);
     currentUser = new User(userById)
+    // helper funtion to show manager dashboard
   }else {
     alert("Appologies, the username or password you have entered does not match any we have on file. Please try again!")
   }
 }
-// USER LOGIN:
-// takin user input from both imput fields
-// evaluate IF the value for username is === 'manager'
-// ... and the password entry is === 'overlook2020'
-// if yes, instantiate the manager
-// ELSEIF slice out the first 8ish characters and check if that === 'customer' and the password === overlook2020
-    // slice 8 to get everything after the eith charcater
-    // grab user info from sever and compair ids
-    // if they are the same number then make object with the id and name properties
-    // pass object into user instantiation
-// else alert wrong info please try again
