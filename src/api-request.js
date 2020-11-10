8c4557c2793c55e20707522d07340159a5340f4c
Cameron Romo <cromo1225@gmail.com>
2020-11-10 10:39:42 -0700

    Refactor deleteBooking() to take in correct body

--- src/api-request.js
+++ src/api-request.js
@@ -30,12 +30,15 @@ const apiRequest = {
   },
 
   deleteBooking(booking) {
+    console.log(booking);
     fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings', {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(booking),
+      body: JSON.stringify({
+        'id': parseInt(booking)
+      }),
     })
     .then(response => response.json())
     .catch(error => console.log(error));
